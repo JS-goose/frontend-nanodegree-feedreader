6f9a8ad09b1e8986934b1ca8b7fb8a3defbdf3b9
Jonathan Sexton <joncsexton@gmail.com>
2018-08-15 09:46:48 -0500

    updated gulpfile and still working on the Name test

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var browserSync = require("browser-sync").create();
 
 // Static server
 gulp.task("browser-sync", function() {
- browserSync.init({
-   server: {
-     baseDir: "./",
-   }
- });
-     gulp.watch("*.html,* .js, *.css").on('change', browserSync.reload);
+browserSync.init({
+  server: {
+    baseDir: "./",
+  }
+});
+    gulp.watch(['./*.html' ,'js/*.js', 'css/*.css']).on('change', browserSync.reload);
 });
