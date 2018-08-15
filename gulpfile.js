7422e07e95e7e5d941059c9058b60756499b6591
Jonathan Sexton <joncsexton@gmail.com>
2018-08-15 08:38:05 -0500

    working on feed name looping

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
 
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+gulp.task("browser-sync", function() {
+ browserSync.init({
+   server: {
+     baseDir: "./",
+   }
+ });
+     gulp.watch("*.html,* .js, *.css").on('change', browserSync.reload);
 });
